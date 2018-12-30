2e803ce6e5011f4ea2bb2d587dfbcbcc8b028af8
matheuslimat <matheuslimat@gmail.com>
2018-12-30 19:22:26 -0200

    Banco de dados H2

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -15,15 +15,11 @@ public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
-		
 		Categoria cat1 = new Categoria(1, "Informática");
 		Categoria cat2 = new Categoria(2, "Escritório");
-		
 		List<Categoria> lista = new ArrayList<>();
-		
 		lista.add(cat1);
 		lista.add(cat2);
-		
 		return lista;
 	}
 
