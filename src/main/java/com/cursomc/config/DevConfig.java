c0bffb54c33f041934d3d0515215a5b0c12b5ad8
matheuslimat <matheuslimat@gmail.com>
2019-07-07 16:48:53 -0300

    Enviando email via SMTP

--- src/main/java/com/cursomc/config/DevConfig.java
+++ src/main/java/com/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.cursomc.services.DBservice;
+import com.cursomc.services.EmailService;
+import com.cursomc.services.SmtEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtEmailService();
 	}
 }
--- src/main/java/com/cursomc/services/DBservice.java
+++ src/main/java/com/cursomc/services/DBservice.java
@@ -138,7 +138,7 @@ public class DBservice {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "12134847405", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "benissecosta@gmail.com", "12134847405", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("33427712", "24906872"));
 
